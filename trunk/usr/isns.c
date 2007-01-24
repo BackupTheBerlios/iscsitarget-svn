f92ad75642a40690e4d922dea19b8d4b0885b0e5
tomo <tomo@48a34bb2-7106-0410-bc49-8aa7273d22a1>
2007-01-24 12:27:32 +0000

    iSNS define fix for big-endian architectures (From Vishal Study).

--- trunk/usr/isns.c
+++ trunk/usr/isns.c
@@ -223,7 +223,7 @@ static int isns_scn_deregister(char *name)
 	x = (x & 0x0000ffff) << 16 | (x & 0xffff0000) >> 16;	\
 }
 #else
-#define set_scn_flag(x)
+#define set_scn_flag(x) (x)
 #endif
 
 static int isns_scn_register(void)
